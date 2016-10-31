a47384cf8c1c2511654796832d102a82b92bf78c
xiamijun <171571466@qq.com>
2016-10-31 18:08:17 +0800

    xiamijun

--- static/newsAction.php
+++ static/newsAction.php
@@ -1,27 +1,51 @@
 <?php
-$id=$_GET['id'];
+header("content-type:text/html;charset:utf-8");
 
-$html_filename="news_id".$id.".html";
-
-//这里我们可以判断该新闻对应的html文件是否存在了
-//这里还有一个问题.如何保持html页面和数据库信息的同步问题
-//1. 在更新时，同时修改->这里涉及到模板替换
-//2.如果对及时性要求不高，通常可以这样处理 30s
-//3.这样还有一个问题，就是我们的网页地址，仍然不是静态网址,-》这里涉及到伪静态
-//4.还有就是静态页面和数据库的及时同步问题?->涉及到真静态的模板替换
-if(file_exists($html_filename)&&time()-filemtime($html_filename)<30){
-    //如果该静态html页面存在我们就直接输出.
-    echo file_get_contents($html_filename);
-    exit();
+function replace($row,$title,$content){
+    //含义是 用 $title的内容替换 $row中的 %title%
+    $row=str_replace('%title%',$title,$row);
+    $row=str_replace('%content%',$content,$row);
+    return $row;
 }
 
-$conn=mysqli_connect('localhost','root','','static','3306');
-if(!$conn){
-    die('连接失败');
-}
-$sql="select * from news where is=$id";
-$res=mysqli_query($conn,$sql);
-if($row=mysqli_fetch_assoc($res)){
-    ob_start();
-    header("content-type:text/html;charset:utf-8");
+//处理添加、修改、删除请求
+$oper=$_REQUEST['oper'];
+
+if($oper=='add'){
+    $title=$_POST['title'];
+    $content=$_POST['content'];
+
+    //1.把数据放入到mysql, 同时创建一个html
+
+    $conn=mysqli_connect('localhost','root','','static','3306');
+    if(!$conn){
+        die('连接失败');
+    }
+    $sql="insert into news(title,content) values('$title','$content')";
+    if(mysqli_query($conn,$sql)){
+        //获取刚刚插入数据的id号
+        $id=mysqli_insert_id($conn);
+        $html_filename="news_id".$id.".html";
+
+        //创建html文件
+        $fp_tmp=fopen('template.tpl','r');
+        $fp_html_file=fopen($html_filename,'w');
+
+        //思路->tmp->html 逐行读取template.tpl文件，然后逐行替换
+        while(!feof($fp_tmp)){
+            //读取一行.
+            $row=fgets($fp_tmp);
+            //替换(小函数)
+            $new_row=replace($row,$title,$content);
+            //把替换后的一行写入到html文件
+            fwrite($fp_html_file,$new_row);
+        }
+
+        //关闭文件流
+        fclose($fp_tmp);
+        fclose($fp_html_file);
+
+        echo "添加到数据库并成功创建html文件<a href='news_list.php'>返回列表</a>";
+    }
+    mysqli_close($conn);
 }
--- static/news_list.php
+++ static/news_list.php
@@ -12,7 +12,7 @@ echo "<a href='add_news.html'>添加新闻</a><hr/>";
 echo "<table>";
 echo "<tr><td>id</td><td>标题</td><td>查看详情</td></tr>";
 while($row=mysqli_fetch_assoc($res)){
-    echo "<tr><td>{$row['id']}</td><td>{$row['title']}</td><td><a>查看详情</a></td></tr>";
+    echo "<tr><td>{$row['id']}</td><td>{$row['title']}</td><td><a href='news_id{$row['id']}.html'>查看详情</a></td></tr>";
 }
 echo "</table>";
 mysqli_free_result($res);
